8c72359296e40bbb5670041f73406a5c5e040491
ashik <ashik191022@gmail.com>
2025-06-18 22:20:00 +0600

    Updates

--- assets/PageNotFound-2d81a6fb.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-b2c38c93.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
---
+++ assets/PageNotFound-7242f810.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-193b4f6b.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
